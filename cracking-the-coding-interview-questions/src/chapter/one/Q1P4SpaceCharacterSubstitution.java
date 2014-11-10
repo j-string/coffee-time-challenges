b6a58fbd23ee29520a0810ecae1fc3bc7e9928f7
j-string <the.stringa@gmail.com>
2014-11-09 21:35:47 -0600

    added new, simpler solution

--- cracking-the-coding-interview-questions/src/chapter/one/Q1P4SpaceCharacterSubstitution.java
+++ cracking-the-coding-interview-questions/src/chapter/one/Q1P4SpaceCharacterSubstitution.java
@@ -1,34 +1,38 @@
 package chapter.one;
 
+import java.util.logging.Logger;
+
 /**
  * 
  * @author Justin Stringer
  */
 public class Q1P4SpaceCharacterSubstitution {
 
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
+
 	/**
-	 * @param inputString
+	 * @param inputChars
+	 * @param trueLength
 	 * @return
 	 */
-	public String replaceSpaceCharacters(String inputString) {
+	public char[] replaceSpaceCharacters(char[] inputChars, int trueLength) {
 
-		if (inputString == null) {
+		if (inputChars == null) {
 			return null;
-		} else if (inputString.isEmpty() || !inputString.contains(" ")) {
-			return inputString;
+		} else if (inputChars.length == 0) {
+			return inputChars;
 		} else {
-			char[] inputChars = inputString.toCharArray();
-			for (int i = 0; i < inputChars.length; i++) {
-				if (inputChars[i] == ' ')
-				{
-					inputChars = this.shiftArrayElements(inputChars, i, 3);
+			for (int i = 0; i < trueLength; i++) {
+				if (inputChars[i] == ' ') {
+					inputChars = this.shiftArrayElements(inputChars, i, 2);
 					inputChars[i] = '%';
-					inputChars[i+1] = '2';
-					inputChars[i+2] = '0';
+					inputChars[i + 1] = '2';
+					inputChars[i + 2] = '0';
+					trueLength += 2;
 				}
 			}
 
-			return new String(inputChars);
+			return inputChars;
 		}
 	}
 
@@ -37,20 +41,74 @@ public class Q1P4SpaceCharacterSubstitution {
 	 * @param shiftFrom
 	 * @param numberOfPositions
 	 */
-	char[] shiftArrayElements(char[] inputChars, int shiftFrom,
-			int numberOfPositions) {
-		if (inputChars == null || inputChars.length == 0
-				|| numberOfPositions == 0) {
+	char[] shiftArrayElements(char[] inputChars, int shiftFrom, int numberOfPositions) {
+		if (inputChars == null || inputChars.length == 0 || numberOfPositions == 0) {
 			return inputChars;
 		} else if (shiftFrom >= inputChars.length - 1) {
-			throw new RuntimeException(
-					"Cannot shift from the end of the array or beyond!");
+			throw new RuntimeException("Cannot shift from the end of the array or beyond!");
 		} else {
-			for (int i = inputChars.length - 1; i >= shiftFrom
-					+ numberOfPositions; i--) {
+			for (int i = inputChars.length - 1; i >= shiftFrom + numberOfPositions; i--) {
 				inputChars[i] = inputChars[i - numberOfPositions];
 			}
 			return inputChars;
 		}
 	}
+
+	/**
+	 * Makes an initial pass through the array to count all the spaces before
+	 * trueLength is reached. Makes a second pass through the array, starting at
+	 * the end, and working backward.
+	 * 
+	 * 2n
+	 * 
+	 * @param inputChars
+	 * @param trueLength
+	 * @return
+	 */
+	public char[] replaceSpaceCharactersSimpler(char[] inputChars, int trueLength) {
+
+		logger.finest("trueLength=" + trueLength);
+
+		if (inputChars == null || inputChars.length == 0 || trueLength == 0) {
+			return inputChars;
+		}
+
+		logger.finest("inputChars.length=" + inputChars.length);
+
+		int numberOfSpaces = 0;
+		for (int i = 0; i < trueLength; i++) {
+			if (inputChars[i] == ' ') {
+				numberOfSpaces++;
+			}
+		}
+
+		logger.finest("numberOfSpaces=" + numberOfSpaces);
+
+		if (numberOfSpaces == 0) {
+			return inputChars;
+		}
+
+		int newTrueLength = trueLength + numberOfSpaces * 2;
+
+		logger.finest("newTrueLength=" + newTrueLength);
+
+		if (newTrueLength > inputChars.length) {
+			throw new RuntimeException("Not enough space in char array to convert all space characters to \"%20\"\'s");
+		}
+
+		for (int j = trueLength - 1, k = newTrueLength - 1; j >= 0; j--) {
+			if (inputChars[j] == ' ') {
+				inputChars[k] = '0';
+				inputChars[k - 1] = '2';
+				inputChars[k - 2] = '%';
+				k -= 3;
+			} else {
+				inputChars[k] = inputChars[j];
+				k--;
+			}
+		}
+
+		return inputChars;
+	}
+
 }
--- cracking-the-coding-interview-questions/test/chapter/one/Q1P4SpaceCharacterSubstitutionTest.java
+++ cracking-the-coding-interview-questions/test/chapter/one/Q1P4SpaceCharacterSubstitutionTest.java
@@ -1,9 +1,14 @@
 package chapter.one;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -13,6 +18,18 @@ import org.junit.Test;
 public class Q1P4SpaceCharacterSubstitutionTest {
 
 	private Q1P4SpaceCharacterSubstitution testQ1P4SpaceCharacterSubstitution = null;
+
+	/**
+	 * @throws Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Handler[] handlers = Logger.getLogger("").getHandlers();
+		for (int index = 0; index < handlers.length; index++) {
+			handlers[index].setLevel(Level.FINEST);
+		}
+		Logger.getLogger(Q1P4SpaceCharacterSubstitution.class.getName()).setLevel(Level.FINEST);
+	}
 
 	/**
 	 * @throws java.lang.Exception
@@ -37,9 +54,7 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	 */
 	@Test
 	public void testShiftArrayElementForNull() {
-		assertArrayEquals(null,
-				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(
-						null, 1, 1));
+		assertArrayEquals(null, this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(null, 1, 1));
 	}
 
 	/**
@@ -49,9 +64,7 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	 */
 	@Test
 	public void testShiftArrayElementForEmptyArray() {
-		assertArrayEquals(new char[0],
-				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(
-						new char[0], 1, 1));
+		assertArrayEquals(new char[0], this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[0], 1, 1));
 	}
 
 	/**
@@ -62,8 +75,8 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	@Test
 	public void testShiftArrayElementForZeroNumberOfPositions() {
 		assertArrayEquals(new char[] { 'a', 'b', ' ', ' ', ' ', ' ' },
-				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(
-						new char[] { 'a', 'b', ' ', ' ', ' ', ' ' }, 1, 0));
+				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] { 'a', 'b', ' ', ' ', ' ', ' ' },
+						1, 0));
 	}
 
 	/**
@@ -71,11 +84,10 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#shiftArrayElements(char[], int, int)}
 	 * .
 	 */
-	@Test(expected = Exception.class)
+	@Test(expected = RuntimeException.class)
 	public void testShiftArrayElementForShiftFromAtEndOfArray() {
-		this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] {
-				'a', 'b', ' ', ' ', ' ', ' ' }, new char[] { 'a', 'b', ' ',
-				' ', ' ', ' ' }.length - 1, 1);
+		this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] { 'a', 'b', ' ', ' ', ' ', ' ' },
+				new char[] { 'a', 'b', ' ', ' ', ' ', ' ' }.length - 1, 1);
 	}
 
 	/**
@@ -83,11 +95,10 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#shiftArrayElements(char[], int, int)}
 	 * .
 	 */
-	@Test(expected = Exception.class)
+	@Test(expected = RuntimeException.class)
 	public void testShiftArrayElementForShiftFromBeyondEndOfArray() {
-		this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] {
-				'a', 'b', ' ', ' ', ' ', ' ' }, new char[] { 'a', 'b', ' ',
-				' ', ' ', ' ' }.length + 1, 1);
+		this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] { 'a', 'b', ' ', ' ', ' ', ' ' },
+				new char[] { 'a', 'b', ' ', ' ', ' ', ' ' }.length + 1, 1);
 	}
 
 	/**
@@ -98,8 +109,8 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	@Test
 	public void testShiftArrayElementFor1And1() {
 		assertArrayEquals(new char[] { 'a', 'b', 'b', ' ', ' ', ' ' },
-				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(
-						new char[] { 'a', 'b', ' ', ' ', ' ', ' ' }, 1, 1));
+				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] { 'a', 'b', ' ', ' ', ' ', ' ' },
+						1, 1));
 	}
 
 	/**
@@ -109,9 +120,10 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	 */
 	@Test
 	public void testShiftArrayElementFor0And1() {
-		assertArrayEquals(new char[] { 'a', 'a', 'b', 'c', ' ', ' ', ' ' },
-				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(
-						new char[] { 'a', 'b', 'c', ' ', ' ', ' ', ' ' }, 0, 1));
+		assertArrayEquals(
+				new char[] { 'a', 'a', 'b', 'c', ' ', ' ', ' ' },
+				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] { 'a', 'b', 'c', ' ', ' ', ' ',
+						' ' }, 0, 1));
 	}
 
 	/**
@@ -121,9 +133,10 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	 */
 	@Test
 	public void testShiftArrayElementFor0And3() {
-		assertArrayEquals(new char[] { 'a', 'b', 'c', 'a', 'b', 'c', ' ' },
-				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(
-						new char[] { 'a', 'b', 'c', ' ', ' ', ' ', ' ' }, 0, 3));
+		assertArrayEquals(
+				new char[] { 'a', 'b', 'c', 'a', 'b', 'c', ' ' },
+				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] { 'a', 'b', 'c', ' ', ' ', ' ',
+						' ' }, 0, 3));
 	}
 
 	/**
@@ -133,42 +146,155 @@ public class Q1P4SpaceCharacterSubstitutionTest {
 	 */
 	@Test
 	public void testShiftArrayElementFor2And3() {
-		assertArrayEquals(new char[] { 'a', 'b', 'c', ' ', ' ', 'c', ' ' },
-				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(
-						new char[] { 'a', 'b', 'c', ' ', ' ', ' ', ' ' }, 2, 3));
+		assertArrayEquals(
+				new char[] { 'a', 'b', 'c', ' ', ' ', 'c', ' ' },
+				this.testQ1P4SpaceCharacterSubstitution.shiftArrayElements(new char[] { 'a', 'b', 'c', ' ', ' ', ' ',
+						' ' }, 2, 3));
 	}
 
 	/**
 	 * Test method for
-	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharacters(java.lang.String)}
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharacters(char[], int)}
 	 * .
 	 */
 	@Test
-	public void testReplaceSpaceCharacters() {
-		char[] testCharArray = new char[10];
-		testCharArray[0] = 'a';
-		testCharArray[1] = ' ';
-		testCharArray[2] = 'b';
-		// assertEquals(new String("a%20b"),
-		// this.testQ1P4SpaceCharacterSubstitution
-		// .replaceSpaceCharacters(new String(testCharArray)));
-		System.out.println(new String(testCharArray));
-		System.out.println(this.testQ1P4SpaceCharacterSubstitution
-				.replaceSpaceCharacters(new String(testCharArray)));
-		
-		
+	public void testReplaceSpaceCharactersWithOneSpace() {
+		assertArrayEquals(new char[] { 'a', '%', '2', '0', 'b' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharacters(new char[] { 'a', ' ', 'b', ' ', ' ' },
+						3));
 	}
 
 	/**
 	 * Test method for
-	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharacters(java.lang.String)}
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharacters(char[], int)}
 	 * .
 	 */
 	@Test
-	public void testReplace1SpaceCharacters() {
-		assertEquals("a%20b  ",
-				this.testQ1P4SpaceCharacterSubstitution
-						.replaceSpaceCharacters("a b    "));
+	public void testReplaceSpaceCharactersWithTwoSpaces() {
+		assertArrayEquals(
+				new char[] { 'a', '%', '2', '0', '%', '2', '0', 'b' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharacters(new char[] { 'a', ' ', ' ', 'b', ' ',
+						' ', ' ', ' ' }, 4));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerForNull() {
+		assertArrayEquals(null, this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(null, 4));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerForArrayOfLength0() {
+		assertArrayEquals(new char[0],
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(new char[0], 4));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerForTrueLengthOf0() {
+		assertArrayEquals(new char[] { 'a', ' ', ' ', 'b' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(
+						new char[] { 'a', ' ', ' ', 'b' }, 0));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerWithNoSpaces() {
+		assertArrayEquals(new char[] { 'a', 'b', 'c', 'd' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(
+						new char[] { 'a', 'b', 'c', 'd' }, 4));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test(expected = RuntimeException.class)
+	public void testReplaceSpaceCharactersSimplerWithNotEnoughSpaceToReplace() {
+		this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(new char[] { 'a', ' ', 'b' }, 3);
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerWithOneSpace() {
+		assertArrayEquals(
+				new char[] { 'a', '%', '2', '0', 'b' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(new char[] { 'a', ' ', 'b', ' ',
+						' ' }, 3));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerWithTwoSpaces() {
+		assertArrayEquals(
+				new char[] { 'a', '%', '2', '0', '%', '2', '0', 'b' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(new char[] { 'a', ' ', ' ', 'b',
+						' ', ' ', ' ', ' ' }, 4));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerWithTwoSeparatedSpaces() {
+		assertArrayEquals(
+				new char[] { 'a', '%', '2', '0', 'b', '%', '2', '0', 'c' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(new char[] { 'a', ' ', 'b', ' ',
+						'c', ' ', ' ', ' ', ' ' }, 5));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerWithInitialSpace() {
+		assertArrayEquals(
+				new char[] { '%', '2', '0', 'a', 'b', '%', '2', '0', 'c' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(new char[] { ' ', 'a', 'b', ' ',
+						'c', ' ', ' ', ' ', ' ' }, 5));
+	}
+
+	/**
+	 * Test method for
+	 * {@link chapter.one.Q1P4SpaceCharacterSubstitution#replaceSpaceCharactersSimpler(char[], int)}
+	 * .
+	 */
+	@Test
+	public void testReplaceSpaceCharactersSimplerWithEndingSpace() {
+		assertArrayEquals(
+				new char[] { '%', '2', '0', 'a', 'b', 'c', '%', '2', '0' },
+				this.testQ1P4SpaceCharacterSubstitution.replaceSpaceCharactersSimpler(new char[] { ' ', 'a', 'b', 'c',
+						' ', ' ', ' ', ' ', ' ' }, 5));
 	}
 
 }
